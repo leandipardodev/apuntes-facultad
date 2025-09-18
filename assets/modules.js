472c9148e54a38e535127990a562c39044e40412
leandipardodev <leandipardo@gmail.com>
2025-09-18 18:18:59 -0300

    mas optimizaciones

--- assets/modules.js
+++ assets/modules.js
@@ -3,13 +3,14 @@ import fetchStart, { flechas, ventana } from "./fetch-html.js";
 import focus from "./focus.js";
 import rotarHoja from "./rotar-hoja.js";
 import seleccionarSemestre from "./select-semestre.js";
-
 document.addEventListener("DOMContentLoaded", (e) => {
-  fetchStart();
-  flechas();
-  ventana();
   focus();
   rotarHoja("#booklet-rotate");
   seleccionarSemestre();
   cursorStyles();
+  setTimeout(() => {
+    fetchStart();
+    flechas();
+    ventana();
+  }, 0);
 });
